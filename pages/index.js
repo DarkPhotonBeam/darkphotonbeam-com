4f723cc04418d99bb4359b909c0a8328e3d72e7b
Dan Photon <danphoton@yahoo.com>
2021-12-06 11:16:44 +0100

    Even Better Mobile Support

--- pages/index.js
+++ pages/index.js
@@ -140,7 +140,7 @@ export default function Home() {
         // const treble = trebleAvg * _trebleBias;
 
         const scaleOffset = 1;
-        const scaleMultiplier = 30;
+        const scaleMultiplier = document.innerWidth < 1000 ? 20 : 30;
 
         const colorOffset = 40;
         const colorMultiplier = 1;
