c547013365bc2e3122e17d002df9a363881576ee
Dan Photon <danphoton@yahoo.com>
2021-12-06 12:38:18 +0100

    More Effects

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,8 @@ const playlist = [
     '/audio/projectEuroMirVersion03.mp3',
 ];
 
+const barOpacity = 0.3;
+
 const euler = 2.71828182846;
 
 const isInteractingState = atom({
@@ -52,6 +54,11 @@ export default function Home() {
     const [midBias, setMidBias] = useState(1);
     const [trebleBias, setTrebleBias] = useState(1.2);
     const [volumeAvg, setVolumeAvg] = useState(0);
+    const [biasedVolumeAvg, setBiasedVolumeAvg] = useState(0);
+
+    const [bass, setBass] = useState(0);
+    const [mid, setMid] = useState(0);
+    const [treble, setTreble] = useState(0);
 
     const [showMenu, setShowMenu] = useState(false);
     const [currentMenuInterval, setCurrentMenuInterval] = useState(null);
@@ -91,7 +98,7 @@ export default function Home() {
         _audioFile.volume = 1;
         analyser.fftSize = 64
         analyser.maxDecibels = -10;
-        analyser.smoothingTimeConstant = 0.7; // DEFAULT: 0.8
+        analyser.smoothingTimeConstant = 0.75; // DEFAULT: 0.8
         source.connect(audioContext.destination);
         source.connect(analyser);
         _audioFile.play().then(() => {
@@ -133,7 +140,7 @@ export default function Home() {
         const _midBias = midBias;
         const _trebleBias = trebleBias;
 
-        //console.log(`bass bias: ${_bassBias} mid bias: ${midBias} treble bias: ${trebleBias}`);
+        //console.log(`_bass bias: ${_bassBias} _mid bias: ${midBias} _treble bias: ${trebleBias}`);
 
         const bassAvg = getBandAverage(freqData, 0, 1);
         const midAvg = getBandAverage(freqData, 2, 5);
@@ -141,26 +148,33 @@ export default function Home() {
         const _volumeAvg = (bassAvg + midAvg + trebleAvg) / 3;
         setVolumeAvg(_volumeAvg * _volumeAvg / 255);
 
-        const bass = (bassAvg*bassAvg) / 255 * _bassBias;
-        const mid = (midAvg*midAvg) / 255 * _midBias;
-        const treble = (trebleAvg*trebleAvg) / 255 * _trebleBias;
+        const _bass = (bassAvg*bassAvg) / 255 * _bassBias;
+        const _mid = (midAvg*midAvg) / 255 * _midBias;
+        const _treble = (trebleAvg*trebleAvg) / 255 * _trebleBias;
 
-        // const bass = bassAvg * _bassBias;
-        // const mid = midAvg * _midBias;
-        // const treble = trebleAvg * _trebleBias;
+        setBass(_bass);
+        setMid(_mid);
+        setTreble(_treble);
+
+        // const _bass = bassAvg * _bassBias;
+        // const _mid = midAvg * _midBias;
+        // const _treble = trebleAvg * _trebleBias;
 
         const scaleOffset = 1;
         const scaleMultiplier = window.innerWidth/window.innerHeight * 15;
 
         const colorOffset = 40;
         const colorMultiplier = 1;
-        //console.log(`bass: ${bass} mid: ${mid} treble: ${treble}`);
+        //console.log(`_bass: ${_bass} _mid: ${_mid} _treble: ${_treble}`);
 
-        setMeshScale(scaleOffset + ((bass + mid + treble) / (255 * (_bassBias + _midBias + _trebleBias)) * scaleMultiplier));
+        const _biasedVolumeAvg = (_bass + _mid + _treble) / (255 * (_bassBias + _midBias + _trebleBias));
+        setBiasedVolumeAvg(_biasedVolumeAvg);
+
+        setMeshScale(scaleOffset + (_biasedVolumeAvg * scaleMultiplier));
         setMeshColor([
-            colorOffset + parseInt(bass * colorMultiplier - colorOffset),
-            colorOffset + parseInt(treble * colorMultiplier - colorOffset),
-            colorOffset + parseInt(mid * colorMultiplier - colorOffset),
+            colorOffset + parseInt(_bass * colorMultiplier - colorOffset),
+            colorOffset + parseInt(_treble * colorMultiplier - colorOffset),
+            colorOffset + parseInt(_mid * colorMultiplier - colorOffset),
         ]);
         localStorage.setItem('audioCurrentTime', audioFile.currentTime.toString());
 
@@ -177,6 +191,8 @@ export default function Home() {
             requestAnimationFrame(animate);
         }
     }, [audioData])
+
+    const biasedBarOpacity = barOpacity + treble / 255 * (1 - barOpacity);
 
     return (
         <div className={styles.container} style={{cursor: showMenu ? 'initial' : 'none'}} onMouseMove={e => {
@@ -200,15 +216,15 @@ export default function Home() {
                 <pointLight position={[10, 10, 10]}/>
                 {
                     frequencyData.map((amp, i) => (
-                        <Bar key={i} position={[(-(i-6) * 3) - 1.5, 0, -40]} height={amp / 255 * 64} color={`rgb(${parseInt(meshColor[0] * .15)}, ${parseInt(meshColor[1]* .15)}, ${parseInt(meshColor[2]* .15)})`} />
+                        <Bar key={i} position={[(-(i-6) * (6 * (bass / 255))) - (3 * (bass / 255)), 0, -40 - (biasedVolumeAvg * 40)]} height={amp / 255 * 64} color={`rgb(${parseInt(meshColor[0] * biasedBarOpacity)}, ${parseInt(meshColor[1]* biasedBarOpacity)}, ${parseInt(meshColor[2]* biasedBarOpacity)})`} />
                     )).slice(-30, 30).reverse()
                 }
                 {
                     frequencyData.map((amp, i) => (
-                        <Bar key={i} position={[((i-6) * 3) + 1.5, 0, -40]} height={amp / 255 * 64} color={`rgb(${parseInt(meshColor[0] * .15)}, ${parseInt(meshColor[1]* .15)}, ${parseInt(meshColor[2]* .15)})`} />
+                        <Bar key={i} position={[((i-6) * (6 * (bass / 255))) + (3 * (bass / 255)), 0, -40 - (biasedVolumeAvg * 40)]} height={amp / 255 * 64} color={`rgb(${parseInt(meshColor[0] * biasedBarOpacity)}, ${parseInt(meshColor[1]* biasedBarOpacity)}, ${parseInt(meshColor[2]* biasedBarOpacity)})`} />
                     )).slice(-30, 30)
                 }
-                <Box rotationSpeed={volumeAvg * .005} color={`rgb(${meshColor[0]}, ${meshColor[1]}, ${meshColor[2]})`} scale={meshScale} position={[0, 0, -20]}/>
+                <Box rotationSpeed={volumeAvg * .007} color={`rgb(${meshColor[0]}, ${meshColor[1]}, ${meshColor[2]})`} scale={meshScale} position={[0, 0, -20]}/>
                 {/*<EffectComposer>*/}
                 {/*    /!*<DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />*!/*/}
                 {/*    <Bloom luminanceThreshold={0.5} luminanceSmoothing={.5} height={50} />*/}
@@ -314,7 +330,7 @@ function Box(props) {
             // onPointerOver={(event) => hover(true)}
             // onPointerOut={(event) => hover(false)}
             >
-            <octahedronGeometry />
+            <octahedronGeometry args={[1, 0]} />
             <meshStandardMaterial color={hovered ? 'red' : props.color}/>
         </mesh>
     )
